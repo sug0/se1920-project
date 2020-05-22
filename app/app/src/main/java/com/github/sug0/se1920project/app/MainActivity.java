b8f65cb6347f99740d46bce682c71f54b1eead1e
sugoiuguu <sugoiuguu@tfwno.gf>
2020-05-22 17:14:42 +0100

    initial commit of app

---
+++ app/app/src/main/java/com/github/sug0/se1920project/app/MainActivity.java
@@ -0,0 +1,12 @@
+package com.github.sug0.se1920project.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
