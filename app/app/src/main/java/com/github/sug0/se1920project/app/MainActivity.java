77b394fba6bc4c0124ceff669a470c11ed0aa5c4
sugoiuguu <sugoiuguu@tfwno.gf>
2020-05-23 04:30:05 +0100

    why is this not working :(

--- app/app/src/main/java/com/github/sug0/se1920project/app/MainActivity.java
+++ app/app/src/main/java/com/github/sug0/se1920project/app/MainActivity.java
@@ -8,8 +8,24 @@ import android.widget.TextView;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.*;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class MainActivity extends AppCompatActivity {
+    // mqtt client stuff
+    private final AtomicReference<MqttAndroidClient> atomicMqttClient = new AtomicReference<>();
+    private final AtomicBoolean mqttLoaded = new AtomicBoolean();
+
+    private static final String MQTT_CLIENT_PREFIX = "sugo";
+    private static final String MQTT_BROKER = "tcp://broker.hivemq.com:1883";
+    private static final String MQTT_TOPIC = "minecraft/fcup/accelerometer-data";
+
+    // gyroscope stuff
     private SensorManager sensorManager;
     private Sensor gyroscopeSensor;
     private SensorEventListener gyroscopeEventListener;
@@ -19,20 +35,44 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // try to connect to message broker
+        MqttAndroidClient mqttClient = new MqttAndroidClient(getApplicationContext(), MQTT_BROKER, generateClientId());
+        mqttClient.setCallback(new MqttCallbackExtended() {
+            @Override
+            public void connectComplete(boolean reconnect, String serverURI) {
+                mqttLoaded.set(true);
+            }
+
+            @Override
+            public void connectionLost(Throwable cause) {
+                mqttLoaded.set(false);
+            }
+
+            @Override
+            public void messageArrived(String topic, MqttMessage message) throws Exception {
+                // do nothing for now
+            }
+
+            @Override
+            public void deliveryComplete(IMqttDeliveryToken token) {
+                // do nothing for now
+            }
+        });
+        atomicMqttClient.set(mqttClient);
+        connectMqtt();
+
         // load gyroscope
         sensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
 
         if (sensorManager == null) {
-            final Toast toast = Toast.makeText(this, "No sensors found?!", Toast.LENGTH_SHORT);
-            toast.show();
+            toast("Failed to load sensor manager.");
             finish();
         }
 
         gyroscopeSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
 
         if (gyroscopeSensor == null) {
-            final Toast toast = Toast.makeText(this, "No gyroscope found.", Toast.LENGTH_SHORT);
-            toast.show();
+            toast("Failed to load gyroscope.");
             finish();
         }
 
@@ -42,12 +82,26 @@ public class MainActivity extends AppCompatActivity {
                 final float[] sensorValues = event.values;
 
                 final TextView textView = findViewById(R.id.gyroDebug);
-                final String debugText = String.format("%d\n%d\n%d\n",
-                    (int)sensorValues[0],
-                    (int)sensorValues[1],
-                    (int)sensorValues[2]);
+                final String debugText = Arrays.toString(sensorValues);
 
+                // update debug view
                 textView.setText(debugText);
+
+                // send data to mqtt
+                if (mqttLoaded.get()) {
+                    MqttMessage message = new MqttMessage();
+                    message.setQos(0);
+                    message.setRetained(false);
+                    message.setPayload(debugText.getBytes());
+                    try {
+                        MqttAndroidClient mqttClient = atomicMqttClient.get();
+                        if (mqttClient != null) {
+                            mqttClient.publish(MQTT_TOPIC, message);
+                        }
+                    } catch (MqttException e) {
+                        toast("Failed to publish to MQTT: " + e);
+                    }
+                }
             }
 
             @Override
@@ -60,12 +114,48 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
+        connectMqtt();
         sensorManager.registerListener(gyroscopeEventListener, gyroscopeSensor, SensorManager.SENSOR_DELAY_FASTEST);
     }
 
     @Override
     protected void onPause() {
         super.onPause();
+        disconnectMqtt();
         sensorManager.unregisterListener(gyroscopeEventListener);
+    }
+
+    private void connectMqtt() {
+        MqttConnectOptions connectOptions = new MqttConnectOptions();
+        connectOptions.setConnectionTimeout(60);
+        try {
+            MqttAndroidClient mqttClient = atomicMqttClient.get();
+            if (mqttClient != null) {
+                mqttClient.connect(connectOptions);
+            }
+        } catch (MqttException e) {
+            toast("Failed to connect to MQTT: " + e);
+        }
+    }
+
+    private void disconnectMqtt() {
+        try {
+            MqttAndroidClient mqttClient = atomicMqttClient.get();
+            if (mqttClient != null) {
+                mqttClient.disconnect(60*1000);
+            }
+        } catch (MqttException e) {
+            toast("Failed to disconnect from MQTT: " + e);
+        }
+    }
+
+    private static String generateClientId() {
+        final long x = (new Random()).nextLong();
+        return MQTT_CLIENT_PREFIX + "-" + (x & 0xffffffff);
+    }
+
+    private void toast(String message) {
+        final Toast toast = Toast.makeText(this, message, Toast.LENGTH_SHORT);
+        toast.show();
     }
 }
